cc8a1518586728e03accfec41acf206f8198cf43
Jhon Anderson <anderson052594@gmail.com>
2023-08-22 21:03:33 -0500

    registros en base de datos

--- database/migrations/2023_08_19_223334_create_posts_table.php
+++ database/migrations/2023_08_19_223334_create_posts_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('sumary');
+            $table->string('summary');
             $table->text('content');
 
             //foreign keys
