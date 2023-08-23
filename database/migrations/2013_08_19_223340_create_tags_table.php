dc43f7e11cda980dd79920c6af1f3b5f395baf74
Jhon Anderson <anderson052594@gmail.com>
2023-08-22 20:18:43 -0500

    usuarios creados

--- database/migrations/2013_08_19_223340_create_tags_table.php
+++ database/migrations/2013_08_19_223340_create_tags_table.php
@@ -11,13 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tags', function (Blueprint $table) {
             $table->id();
-            $table->string('fullname');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('name');
+            $table->text('description');
+
             
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -27,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tags');
     }
 };
